c2c783e33cbab6470be01c9b76516e1cb4ed6a7d
jibedoubleve <jibedoubleve@gmail.com>
2020-10-10 15:42:43 +0200

    Add project files.

---
+++ Business/Evaluator.cs
@@ -0,0 +1,25 @@
+﻿using CodingSeb.ExpressionEvaluator;
+
+namespace enzo.Business
+{
+    public static class Evaluator
+    {
+        #region Fields
+
+        private static readonly ExpressionEvaluator _evaluator = new ExpressionEvaluator();
+
+        #endregion Fields
+
+        #region Methods
+
+        public static bool Evaluate(string expression, string solution)
+        {
+            var t1 = (int)_evaluator.Evaluate(expression);
+            int.TryParse(solution, out int r1);
+
+            return t1 == r1;
+        }
+
+        #endregion Methods
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using enzo.Business;
+using System.Windows;
+using System.Windows.Media;
+
+namespace enzo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructors
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        private void OnResult(object sender, RoutedEventArgs e)
+        {
+            var t1 = Evaluator.Evaluate(calculus_1.Text, result_1.Text);
+            var t2 = Evaluator.Evaluate(calculus_2.Text, result_2.Text);
+            var t3 = Evaluator.Evaluate(calculus_3.Text, result_3.Text);
+
+            validation_1.Fill = t1 ? Brushes.Green : Brushes.Red;
+            validation_2.Fill = t2 ? Brushes.Green : Brushes.Red;
+            validation_3.Fill = t3 ? Brushes.Green : Brushes.Red;
+
+            photo.Visibility = (t1 && t2 && t3)
+                    ? Visibility.Visible
+                    : Visibility.Collapsed;
+        }
+
+        #endregion Methods
+    }
+}
